ef9573dbadc92b9389bf183030b7b1a5b7106cee
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 16:21:53 -0500

    fuckit

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,10 +1,22 @@
 class UrlsController < ApplicationController
   def create
-    #here we accept params from url shortener form
-    #create the shortened url here?
+    @new_url = Url.shorten(params)
+    if @new_url.save
+    else
+      @errors = @new_url.errors.full_messages
+      @urls = Url.all
+    end
+    render 'index/index'
   end
 
   def destroy
-    #here we need @url to destroy
+    Url.find(params[:id]).destroy
+    render 'index/index'
+  end
+
+  def show
+    url = Url.find_by_shortened_url(params[:short_url])
+    url.update_attributes(:clicks => @url[:clicks] +1 )
+    redirect_to "#{url.original_url}"
   end
 end
