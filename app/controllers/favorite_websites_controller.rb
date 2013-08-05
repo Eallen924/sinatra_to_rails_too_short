fe72ba89d22e090300928bafee8cbed4d28ce3db
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 14:51:47 -0500

    pseudocode controller logic

--- app/controllers/favorite_websites_controller.rb
+++ app/controllers/favorite_websites_controller.rb
@@ -1,3 +1,10 @@
 class FavoriteWebsitesController < ApplicationController
-  
+  def create
+    #here we accept params of user_id and url_id
+    #create fav here?
+  end
+
+  def destroy
+    #here we need a @favorite_url to destroy
+  end
 end
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,5 +1,10 @@
 class UrlsController < ApplicationController
-  def new
-    
+  def create
+    #here we accept params from url shortener form
+    #create the shortened url here?
+  end
+
+  def destroy
+    #here we need @url to destroy
   end
 end
