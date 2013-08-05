169fc44ef2f15e081955c957bffbab46c2738c9d
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 10:45:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TooShort::Application.config.secret_token = 'c2d62420bce366dede6c4a629b16bdef5d99914f83cb67b4e329f025594fd39efc842d46684c305e667979a1ca810b04dd45276e2b83b17daa25a78e0af0b86e'
